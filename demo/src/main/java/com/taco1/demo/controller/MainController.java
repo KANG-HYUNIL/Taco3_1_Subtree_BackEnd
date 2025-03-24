852521d42389bbab18122c380ab5d22c6f27534a
Kang-conqueror <hik8835@daum.net>
2025-03-24 14:28:08 +0900

    add MainController, FileService
    MainController - 앱 단의 주요 요청들을 받아 처리할 Controller 클래스.
    + (POST) /api/image , 앱 단에서 FormData 형태로, 'multipartFile' 라는 key에 파일들을 넣어 보내준다고 가정하고 작성.
    
    FileService - 파일(이미지)들의 전/후처리를 담당하고, 다른 서비스를 호출해 파일을 외부로 전달할 Service 클래스.

--- demo/src/main/java/com/taco1/demo/controller/MainController.java
+++ demo/src/main/java/com/taco1/demo/controller/MainController.java
@@ -1,23 +1,42 @@
 package com.taco1.demo.controller;
 
+import com.taco1.demo.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
 public class MainController {
 
+    private final FileService fileService;
 
-    @PostMapping("/test")
+    @PostMapping("/api/image")
     @ResponseBody
-    public ResponseEntity<?> test() {
-        return new ResponseEntity<>("test", HttpStatus.OK);
+    public ResponseEntity<?> test(@RequestPart("multipartFile") List<MultipartFile> files) {
+
+        try {
+            fileService.SendImageToModel(files); //이미지 파일들 처리 서비스 호출
+
+            //모든 처리 작업 완료 후 데이터를 다시 응답에 돌려줘야 함, fixme
+            return new ResponseEntity<>("test", HttpStatus.OK);
+        } catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+
     }
 
 
 
 }
+
---
+++ demo/src/main/java/com/taco1/demo/service/FileService.java
@@ -0,0 +1,18 @@
+package com.taco1.demo.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileService {
+
+
+    //이미지가 들어있는 배열을 받아서, 필요 시 전처리 후 모델에게 전송하는 메서드
+    public void SendImageToModel(List<MultipartFile> files)
+    {
+
+    }
+
+
+}
